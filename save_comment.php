caf037ceae226b5da3d53572b1d2937b05c4b141
rhsukarna@gmail.com <RoyanHabibie>
2023-11-06 13:04:37 +0700

    update

--- save_comment.php
+++ save_comment.php
@@ -5,7 +5,21 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Lakukan validasi atau penyimpanan ke database sesuai kebutuhan
     // Misalnya, menyimpan ke database MySQL
 
-    echo 'Komentar berhasil disimpan';
+    include 'config.php'; // Sesuaikan dengan nama file koneksi
+
+    $cvId = 1; // Sesuaikan dengan id CV Anda
+    $query = "INSERT INTO comments (cv_id, comment_text) VALUES (?, ?)";
+    $stmt = mysqli_prepare($conn, $query);
+    mysqli_stmt_bind_param($stmt, "is", $cvId, $comment);
+
+    if (mysqli_stmt_execute($stmt)) {
+        echo 'Komentar berhasil disimpan';
+    } else {
+        echo 'Terjadi kesalahan saat menyimpan komentar';
+    }
+
+    mysqli_stmt_close($stmt);
+    mysqli_close($conn);
 } else {
     echo 'Metode tidak diizinkan';
 }
--- script.js
+++ script.js
@@ -1,38 +1,24 @@
 function addComment() {
-  var commentInput = document.getElementById('commentInput');
-  var commentText = commentInput.value;
-  
-  if (commentText.trim() !== '') {
-      var commentsDiv = document.getElementById('comments');
-      var newComment = document.createElement('p');
-      newComment.textContent = commentText;
-      commentsDiv.appendChild(newComment);
-      commentInput.value = '';
+    var commentInput = document.getElementById('commentInput');
+    var commentText = commentInput.value;
+    
+    if (commentText.trim() !== '') {
+        var commentsDiv = document.getElementById('comments');
+        var newComment = document.createElement('div');
+        newComment.className = 'comment';
+        newComment.textContent = commentText;
+        commentsDiv.appendChild(newComment);
+        commentInput.value = '';
 
-      // Kirim data komentar ke server menggunakan AJAX
-      var xhr = new XMLHttpRequest();
-      xhr.open('POST', 'save_comment.php', true);
-      xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-      xhr.onreadystatechange = function() {
-          if (xhr.readyState == 4 && xhr.status == 200) {
-              console.log('Komentar berhasil disimpan');
-          }
-      };
-      xhr.send('comment=' + encodeURIComponent(commentText));
-  }
-}
-
-function likeCV() {
-  alert('CV Disukai!');
-
-  // Kirim data "like" ke server menggunakan AJAX
-  var xhr = new XMLHttpRequest();
-  xhr.open('POST', 'save_like.php', true);
-  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-  xhr.onreadystatechange = function() {
-      if (xhr.readyState == 4 && xhr.status == 200) {
-          console.log('Like berhasil disimpan');
-      }
-  };
-  xhr.send();
+        // Kirim data komentar ke server menggunakan AJAX
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', 'save_comment.php', true);
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                console.log('Komentar berhasil disimpan');
+            }
+        };
+        xhr.send('comment=' + encodeURIComponent(commentText));
+    }
 }
